4ed5aeda4264398e54b474b1e2f37381debae6c4
anisamuema <anisa.muema.school@gmail.com>
2023-09-24 13:34:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers){
+   return drivers=['Antonia', 'Nuru'];
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers=['Amari', 'Mo'];
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(faremultiplier){
+    return function(value){
+        return faremultiplier*value;
+    }
+}
+
+  const fareDoubler = createFareMultiplier(2);
+  const fareTripler = createFareMultiplier(3);
+
+  function selectDifferentDrivers(arrayOfDrivers, func){
+    return func(arrayOfDrivers);
+  }
